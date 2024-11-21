d716457794e9937c6c0297e18bc6e9013b0cad1c
Petr Mikhalicin <pmikhalicin@rutoken.ru>
2024-11-21 12:22:13 +0500

    Improve controll and speed

--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,127 @@
 #include <Arduino.h>
 
-#define ENGINE_MAX_RPM 800 // rotation per minutes
-#define ENGINE_SPR 200 // steps per revolution
-#define ENGINE_MICROSTEPS 16
-#define ENGINE_MPS (60000000 / ENGINE_SPR / ENGINE_MICROSTEPS) // microseconds per step for 1RPM
-
-#define CONTROLLER_MAX_VAL 1024.
-
 #define FILM_DIR_PIN 4
 #define FILM_STEP_PIN 5
 #define FILM_SPEED_CONTROLLER_PIN A0
 
-unsigned long gFilmTimer = 0;
-unsigned long gFilmStepMicros = 0;
+int gFilmStepMicros = 200;
+bool gStop = false;
 
-#define SONAR_TRIGGER_PIN  3
-#define SONAR_ECHO_PIN     2
-#define FILM_MAX_DISTANCE 30
-#define FILM_TRIGGER_DISTANCE 5
+#define CONTROLLER_TRASH_HOLD 10
 
-#define SPEED_UPDATE_TIME 1000
-static_assert(SPEED_UPDATE_TIME > 50);
-unsigned long gSpeedUpdateTimer = 0;
+#define SPEED_UPDATE_TIME 100
 
 #define SHOT_INDICATOR_PIN A2
-#define SHOT_INDICATOR_TRIGGER_VALUE 450
-#define MIN_INDICATOR_DELAY 300
-unsigned long gLastIndicatorTime = 0;
-bool gLastIndicatorValue = false;
+#define SHOT_INDICATOR_TRIGGER_VALUE 500
+#define MIN_INDICATOR_DELAY 20
 
-#define SHOT_PIN 9
+#define SHOT_PIN 6
 #define SHOT_TIME 100
 
-void setup(){
-  ADCSRA = (ADCSRA & 0xf8) | 0x04;
-  pinMode(FILM_DIR_PIN, OUTPUT);
-  pinMode(FILM_STEP_PIN, OUTPUT);
-  pinMode(FILM_SPEED_CONTROLLER_PIN, INPUT);
+void setup() {
+    ADCSRA = (ADCSRA & 0xf8) | 0x04;
+    pinMode(FILM_DIR_PIN, OUTPUT);
+    pinMode(FILM_STEP_PIN, OUTPUT);
+    pinMode(FILM_SPEED_CONTROLLER_PIN, INPUT);
 
-  pinMode(SHOT_INDICATOR_PIN, INPUT);
-  pinMode(SHOT_PIN, OUTPUT);
+    pinMode(SHOT_INDICATOR_PIN, INPUT);
+    pinMode(SHOT_PIN, OUTPUT);
 
-  digitalWrite(FILM_DIR_PIN, HIGH);
+    digitalWrite(FILM_DIR_PIN, HIGH);
 
-  digitalWrite(SHOT_PIN, HIGH);
+    digitalWrite(SHOT_PIN, HIGH);
 }
 
-inline unsigned long getStepMicros(uint8_t pin) {
-  int val = analogRead(pin);
-  int rpm = max(ENGINE_MAX_RPM * min(val, CONTROLLER_MAX_VAL) / CONTROLLER_MAX_VAL, 1);
-  return ENGINE_MPS / rpm;
+inline unsigned getStepMicros(uint8_t pin) {
+    static int gPrevVal = -100;
+
+    int val = analogRead(pin);
+
+    if (abs(gPrevVal - val) < CONTROLLER_TRASH_HOLD)
+        val = gPrevVal;
+    else
+        gPrevVal = val;
+
+    gStop = val > 1024 - 20;
+
+    if (val <= 256)
+        return map(val, 0, 256, 30, 80);
+
+    if (val <= 512)
+        return map(val, 256, 512, 80, 200);
+
+    return map(val, 512, 1024, 200, 1000);
 }
 
 inline void readSpeed() {
-  if (millis() - gSpeedUpdateTimer > SPEED_UPDATE_TIME) {
-    gFilmStepMicros = getStepMicros(FILM_SPEED_CONTROLLER_PIN);
+    uint32_t currentTime = millis();
+    static uint32_t gSpeedUpdateTimer = millis();
 
-    gSpeedUpdateTimer = millis();
-  }
+    if (currentTime - gSpeedUpdateTimer <= SPEED_UPDATE_TIME)
+        return;
+
+    // smooth speed up
+    int newDelay = getStepMicros(FILM_SPEED_CONTROLLER_PIN);
+    if (newDelay < 200 && gFilmStepMicros - newDelay > 5)
+        if (gFilmStepMicros > 200)
+            gFilmStepMicros = 200;
+        else
+            gFilmStepMicros = gFilmStepMicros - 5;
+    else
+        gFilmStepMicros = newDelay;
+
+    gSpeedUpdateTimer = currentTime;
 }
 
 inline void checkShot() {
-  if ((millis() - gLastIndicatorTime) > SHOT_TIME)
-    digitalWrite(SHOT_PIN, HIGH);
+    uint32_t currentTime = millis();
+    static uint32_t gLastIndicatorTime = currentTime;
+    static uint32_t gLastShotTime = currentTime;
+    static bool gLastIndicatorValue = false;
+    static bool gInShot = false;
 
-  bool val = analogRead(SHOT_INDICATOR_PIN) < SHOT_INDICATOR_TRIGGER_VALUE;
+    if (currentTime - gLastShotTime < SHOT_TIME)
+        return;
 
-  if ((millis() - gLastIndicatorTime) > MIN_INDICATOR_DELAY && gLastIndicatorValue != val) {
-    if (gLastIndicatorValue == false) {
-      digitalWrite(SHOT_PIN, LOW);
+    if (gInShot == true) {
+        gInShot = false;
+        digitalWrite(SHOT_PIN, HIGH);
     }
 
-    gLastIndicatorTime = millis();
-    gLastIndicatorValue = !gLastIndicatorValue;
-  }
+    if (currentTime - gLastIndicatorTime < MIN_INDICATOR_DELAY)
+        return;
+
+    gLastIndicatorTime = currentTime;
+
+    bool val = analogRead(SHOT_INDICATOR_PIN) < SHOT_INDICATOR_TRIGGER_VALUE;
+
+    if (gLastIndicatorValue != val) {
+        if (gLastIndicatorValue == false) {
+            gInShot = true;
+            gLastShotTime = currentTime;
+            digitalWrite(SHOT_PIN, LOW);
+        }
+
+        gLastIndicatorValue = val;
+    }
 }
 
 inline void moveEngine() {
-    if((unsigned long)(micros() - gFilmTimer) > gFilmStepMicros) {
-    digitalWrite(FILM_STEP_PIN, LOW);
-    digitalWrite(FILM_STEP_PIN, HIGH);
-    gFilmTimer = micros();
-  }
+    uint32_t currentTime = micros();
+    static uint32_t gEngineTimer = currentTime;
+
+    if ((currentTime - gEngineTimer) > (unsigned)gFilmStepMicros) {
+        digitalWrite(FILM_STEP_PIN, HIGH);
+        digitalWrite(FILM_STEP_PIN, LOW);
+        gEngineTimer = currentTime;
+    }
 }
 
-void loop(){
-  readSpeed();
-  checkShot();
-  moveEngine();
+void loop() {
+    readSpeed();
+    if (gStop)
+        return;
+
+    checkShot();
+    moveEngine();
 }
